d0285d4dcccb5b5c3c1f2eeabb7c0d5d0e30d99d
ojasvisancheti <35330613+ojasvisancheti@users.noreply.github.com>
2021-05-21 18:14:28 +0100

    Update CreateTrainAndTestFolder.py

--- code/train/CreateTrainAndTestFolder.py
+++ code/train/CreateTrainAndTestFolder.py
@@ -2,12 +2,12 @@ import os
 import shutil
 source = "source_path"
 dest = "dest_path"
-TRAIN_PATH = r"D:\PLacement\CGG\U-Net\data2\seep_detection\train_images_256"
-MASK_PATH = r"D:\PLacement\CGG\U-Net\data2\seep_detection\train_masks_256"
-TRAINOUT_PATH = r"D:\PLacement\CGG\U-Net\data2\train\train_images_256"
-MASKOUT_PATH = r"D:\PLacement\CGG\U-Net\data2\train\train_masks_256"
-TRAINOUTTEST_PATH = r"D:\PLacement\CGG\U-Net\data2\test\train_images_256"
-MASKOUTTEST_PATH = r"D:\PLacement\CGG\U-Net\data2\test\train_masks_256"
+TRAIN_PATH = r"Path"
+MASK_PATH = r"Path"
+TRAINOUT_PATH = r"Path"
+MASKOUT_PATH = r"Path"
+TRAINOUTTEST_PATH = r"Path"
+MASKOUTTEST_PATH = r"Path"
 files = os.listdir(TRAIN_PATH)
 n = 0
 os.makedirs(TRAINOUT_PATH, exist_ok=True)
